9bed9268aba73b650bd11d5fa5b7d3171d710404
xime <ximebcn@yahoo.es>
2018-05-06 02:48:38 +0200

    Some fixes in Othello and move computer method implemented.

--- games/othello/engine.go
+++ games/othello/engine.go
@@ -2,7 +2,16 @@ package othello
 
 import (
 	"errors"
+	"math"
+	"bytes"
+	"fmt"
+	"math/rand"
 )
+
+type tuple struct {
+	X int
+	Y int
+}
 
 type board [WIDTH][HEIGHT]int8
 
@@ -16,9 +25,14 @@ func (b *board) Init() {
 	b[4][3], b[3][4] = BLACK, BLACK
 }
 
-type tuple struct {
-	X int
-	Y int
+func (b *board) Clone() *board {
+	var new board
+	for i := 0; i < WIDTH; i++ {
+		for j := 0; j < HEIGHT; j++ {
+			new[i][j] = b[i][j]
+		}
+	}
+	return &new
 }
 
 func (b *board) Count() (blacks int, whites int, empties int) {
@@ -47,6 +61,28 @@ func (b *board) Move(player int8, moveToX int, moveToY int) ([]*tuple, error) {
 		b[position.X][position.Y] = player
 	}
 	return changes, nil
+}
+
+// A very simple and stupid implementation
+func (b *board) ComputerMove(player int8) ([]*tuple, error) {
+	var candidate *tuple
+
+	validMovements := b.ValidMovementsForPlayer(player)
+	if len(validMovements) == 0 {
+		return nil, errors.New("player cannot move")
+	}
+	best := math.MinInt64
+
+	for _, movement := range validMovements {
+		board := b.Clone()
+		board.Move(player, movement.X, movement.Y)
+		heuristic := board.Heuristic(player)
+		if best < heuristic {
+			best = heuristic
+			candidate = movement
+		}
+	}
+	return b.Move(player, candidate.X, candidate.Y)
 }
 
 func (b *board) ValidMovementsForPlayer(player int8) []*tuple {
@@ -80,10 +116,10 @@ func (b *board) evalMoveHoriz(player int8, moveToX int, moveToY int) []*tuple {
 	for _, deltaX := range []int{-1, 1} {
 		acum := make([]*tuple, 0)
 		for x := moveToX + deltaX; x >= 0 && x < WIDTH; x += deltaX {
-			if b[x][moveToY] != player && b[x][moveToY] != EMPTY {
+			if b[x][moveToY] != player && b[x][moveToY] != EMPTY && !b.outOfLimits(x+deltaX, moveToY) {
 				acum = append(acum, &tuple{X: x, Y: moveToY})
 			} else {
-				if b[x][moveToY] == EMPTY {
+				if b[x][moveToY] != player {
 					acum = nil
 				}
 				break
@@ -99,10 +135,10 @@ func (b *board) evalMoveVert(player int8, moveToX int, moveToY int) []*tuple {
 	for _, deltaY := range []int{-1, 1} {
 		acum := make([]*tuple, 0)
 		for y := moveToY + deltaY; y >= 0 && y < HEIGHT; y += deltaY {
-			if b[moveToX][y] != player && b[moveToX][y] != EMPTY {
+			if b[moveToX][y] != player && b[moveToX][y] != EMPTY && !b.outOfLimits(moveToX, y+deltaY) {
 				acum = append(acum, &tuple{X: moveToX, Y: y})
 			} else {
-				if b[moveToX][y] == EMPTY {
+				if b[moveToX][y] != player {
 					acum = nil
 				}
 				break
@@ -119,10 +155,10 @@ func (b *board) evalMoveDiagonal(player int8, moveToX int, moveToY int) []*tuple
 		for _, deltaY := range []int{-1, 1} {
 			acum := make([]*tuple, 0)
 			for x, y := moveToX+deltaX, moveToY+deltaY; x >= 0 && x < WIDTH && y >= 0 && y < HEIGHT; x, y = x+deltaX, y+deltaY {
-				if b[x][y] != player && b[x][y] != EMPTY {
+				if b[x][y] != player && b[x][y] != EMPTY && !b.outOfLimits(x+deltaX, y+deltaY) {
 					acum = append(acum, &tuple{X: x, Y: y})
 				} else {
-					if b[x][y] == EMPTY {
+					if b[x][y] != player {
 						acum = nil
 					}
 					break
@@ -132,4 +168,79 @@ func (b *board) evalMoveDiagonal(player int8, moveToX int, moveToY int) []*tuple
 		}
 	}
 	return eats
+}
+
+func (b *board) Heuristic(player int8) int {
+	heuristic := 0
+	for x := 0; x < WIDTH; x++ {
+		for y := 0; y < HEIGHT; y++ {
+			value := 1
+			if b[x][y] != EMPTY {
+				if b.isSide(x, y) {
+					if b.isEdge(x, y) {
+						value = 100
+					} else if b.isNearEdge(x, y) {
+						value = 5
+					} else {
+						value = 10
+					}
+				}
+			}
+			if b[x][y] == player {
+				heuristic += value
+			} else {
+				heuristic -= value
+			}
+		}
+	}
+	return heuristic + rand.Intn(30) - 15
+}
+
+func (b *board) outOfLimits(x, y int) bool {
+	return x < 0 || x > WIDTH-1 || y < 0 || y > HEIGHT-1
+}
+
+func (b *board) isEdge(x, y int) bool {
+	return x == 0 && y == 0 || x == WIDTH-1 && y == 0 || x == WIDTH-1 && y == HEIGHT-1 || x == 0 && y == HEIGHT-1
+}
+
+func (b *board) isNearEdge(x, y int) bool {
+	for _, deltaX := range []int{-1, 0, 1} {
+		for _, deltaY := range []int{-1, 0, 1} {
+			if deltaX == 0 && deltaY == 0 {
+				continue
+			}
+			if b.isEdge(x+deltaX, y+deltaY) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func (b *board) isSide(x, y int) bool {
+	return x == 0 || x == WIDTH-1 || y == 0 || y == HEIGHT-1
+}
+
+func (b *board) Dump() []byte {
+	var buf bytes.Buffer
+	var c string
+
+	for j := 0; j < HEIGHT; j++ {
+		for i := 0; i < WIDTH; i++ {
+
+			switch b[i][j] {
+			case BLACK:
+				c = "0"
+			case WHITE:
+				c = "X"
+			default:
+				c = "."
+			}
+
+			buf.WriteString(fmt.Sprintf(" %s ", c))
+		}
+		buf.WriteString("\n")
+	}
+	return buf.Bytes()
 }
--- games/othello/othello_test.go
+++ games/othello/othello_test.go
@@ -2,6 +2,9 @@ package othello
 
 import (
 	"testing"
+	"fmt"
+	"math/rand"
+	"time"
 )
 
 func TestCanMove(t *testing.T) {
@@ -128,4 +131,225 @@ func TestValidMovementsForPlayer(t *testing.T) {
 			t.Errorf("[%d,%d] is not a valid movement", mov.X, mov.Y)
 		}
 	}
+}
+
+func TestValidMovementsForPlayerDiagonal(t *testing.T) {
+	var board board
+	board.Init()
+	board[0][2] = WHITE
+	board[2][2] = WHITE
+
+	board[1][3] = WHITE
+	board[2][3] = BLACK
+	board[3][3] = BLACK
+	board[4][3] = BLACK
+
+	board[2][4] = WHITE
+	board[3][4] = WHITE
+	board[4][4] = WHITE
+
+	inArray := func(p *tuple, l []*tuple) bool {
+		for _, c := range l {
+			if c.X == p.X && c.Y == p.Y {
+				return true
+			}
+		}
+		return false
+	}
+
+	expected := []*tuple{
+		&tuple{3, 4},
+		&tuple{3, 5},
+	}
+
+	changes, err := board.Move(BLACK, 3, 5)
+	if err != nil {
+		t.Error(err)
+	}
+
+
+	for _, mov := range changes {
+		if !inArray(mov, expected) {
+			t.Errorf("[%d,%d] shouldn't change", mov.X, mov.Y)
+		}
+	}
+
+}
+
+func TestBoard_Clone(t *testing.T) {
+	var origin board
+
+	origin.Init()
+	dest := origin.Clone()
+	for i := 0; i < WIDTH; i++ {
+		for j := 0; j < HEIGHT; j++ {
+			if origin[i][j] != dest[i][j] {
+				t.Error("Boards differ.")
+			}
+		}
+	}
+}
+
+func TestBoard_IsEdge(t *testing.T) {
+	var b board
+	var edges = []tuple{{0, 0}, {0, HEIGHT - 1}, {WIDTH - 1, HEIGHT - 1}, {WIDTH - 1, 0}}
+
+	for _, p := range edges {
+		if !b.isEdge(p.X, p.Y) {
+			t.Errorf("[%d,%d] should be an edge.", p.X, p.Y)
+		}
+	}
+
+	for i := 0; i < WIDTH; i++ {
+		for j := 0; j < HEIGHT; j++ {
+			if !inArray(tuple{i, j}, edges) {
+				if b.isEdge(i, j) {
+					t.Errorf("[%d,%d] shouldn't be an edge.", i, j)
+
+				}
+			}
+		}
+	}
+}
+
+func TestBoard_IsSide(t *testing.T) {
+	var b board
+	var sides = []tuple{
+		{0, 0},
+		{1, 0},
+		{2, 0},
+		{3, 0},
+		{4, 0},
+		{5, 0},
+		{6, 0},
+		{7, 0},
+
+		{0, 7},
+		{1, 7},
+		{2, 7},
+		{3, 7},
+		{4, 7},
+		{5, 7},
+		{6, 7},
+		{7, 7},
+
+		{0, 0},
+		{0, 1},
+		{0, 2},
+		{0, 3},
+		{0, 4},
+		{0, 5},
+		{0, 6},
+		{0, 7},
+
+		{7, 0},
+		{7, 1},
+		{7, 2},
+		{7, 3},
+		{7, 4},
+		{7, 5},
+		{7, 6},
+		{7, 7},
+	}
+
+	for _, p := range sides {
+		if !b.isSide(p.X, p.Y) {
+			t.Errorf("[%d,%d] should be an side.", p.X, p.Y)
+		}
+	}
+
+	for i := 0; i < WIDTH; i++ {
+		for j := 0; j < HEIGHT; j++ {
+			if !inArray(tuple{i, j}, sides) {
+				if b.isEdge(i, j) {
+					t.Errorf("[%d,%d] shouldn't be an side.", i, j)
+
+				}
+			}
+		}
+	}
+}
+
+func TestBoard_IsNearEdge(t *testing.T) {
+	var b board
+	var neardEdge = []tuple{
+		{1, 0},
+		{1, 1},
+		{0, 1},
+
+		{0, 6},
+		{1, 6},
+		{1, 7},
+
+		{6, 0},
+		{6, 1},
+		{7, 1},
+
+		{6, 6},
+		{6, 7},
+		{7, 6},
+	}
+
+	for _, p := range neardEdge {
+		if !b.isNearEdge(p.X, p.Y) {
+			t.Errorf("[%d,%d] should be a near edge.", p.X, p.Y)
+		}
+	}
+
+	for i := 0; i < WIDTH; i++ {
+		for j := 0; j < HEIGHT; j++ {
+			if !inArray(tuple{i, j}, neardEdge) {
+				if b.isNearEdge(i, j) {
+					t.Errorf("[%d,%d] shouldn't be a near edge.", i, j)
+
+				}
+			}
+		}
+	}
+}
+
+func TestGame_Ends(t *testing.T) {
+	var board board
+
+	rand.Seed(time.Now().Unix())
+	board.Init()
+
+	for {
+		whiteError := false
+		blackError := false
+		fmt.Println("WHITE PLAYS:")
+		_, err := board.ComputerMove(WHITE)
+		if err != nil {
+			fmt.Println(err)
+			whiteError = true
+		}
+		fmt.Print(string(board.Dump()))
+		w, b, e := board.Count()
+		fmt.Printf("WHITES:%d, BLACKS:%d, EMPTIES:%d\n\n", w, b, e)
+
+		fmt.Println("BLACK PLAYS:")
+		board.ComputerMove(BLACK)
+		if err != nil {
+			fmt.Println(err)
+			blackError = true
+		}
+		fmt.Print(string(board.Dump()))
+		w, b, e = board.Count()
+		fmt.Printf("WHITES:%d, BLACKS:%d, EMPTIES:%d\n\n", w, b, e)
+
+		if whiteError && blackError {
+			return
+		}
+
+	}
+
+}
+
+func inArray(val tuple, tuples []tuple) bool {
+	for _, t := range tuples {
+		if val.X == t.X && val.Y == t.Y {
+			return true
+		}
+	}
+	return false
 }
