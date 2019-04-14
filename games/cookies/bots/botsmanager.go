7fbd66450f068414594cdcba1c143c8e235ff5b1
xime <ximebcn@yahoo.es>
2019-04-14 19:53:45 +0200

    Forgot to submit botsmanager.go

---
+++ games/cookies/bots/botsmanager.go
@@ -0,0 +1,30 @@
+package bots
+
+import (
+	"github.com/x1m3/elixir/games/cookies"
+	"log"
+	"time"
+)
+
+type BotsManager struct {
+	game *cookies.Game
+}
+func NewBotsManager(g *cookies.Game) *BotsManager {
+	return &BotsManager{game:g}
+}
+
+func (m *BotsManager) Init() {
+	t := time.NewTicker(500 * time.Millisecond)
+	for {
+		<- t.C
+		go func() {
+			bot := New(m.game, NewDummyBotAgent(200, 200))
+			log.Println("Bot started")
+			if err := bot.Run(); err != nil {
+				log.Println(err)
+				bot.Destroy()
+				return
+			}
+		}()
+	}
+}
